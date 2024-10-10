84a479542831ed239a1eca0696f899b8778d983d
Zeb5 <blackburn.ze@oundleschool.org.uk>
2024-10-10 12:48:00 +0100

    help

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <?php
+            echo "ooooooooooo<br>";
+            echo "o  #   #  o<br>";
+            echo "o    #    o<br>";
+            echo "o  #   #  o<br>";
+            echo "ooooooooooo<br>";
+        ?>
+    </body>
+</html>
